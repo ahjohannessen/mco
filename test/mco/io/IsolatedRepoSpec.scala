863cf857b10ce84593a80c143b7d7e9c309f27d3
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2016-09-10 14:05:36 +0300

    Added IsolatedRepo test stubs

--- test/mco/io/IsolatedRepoSpec.scala
+++ test/mco/io/IsolatedRepoSpec.scala
@@ -3,11 +3,21 @@ package mco.io
 import mco.UnitSpec
 
 class IsolatedRepoSpec extends UnitSpec {
-  "IsolatedRepo companion" should "create a repository given a folder" in {
-    ???
-  }
+  "IsolatedRepo companion" should "create a repository given a folder" in { ??? }
 
-  it should "preload created repository with existing data" in {
-    ???
-  }
+  it should "preload created repository with existing data" in { ??? }
+
+  "IsolatedRepo#state" should "just be Unit" in { ??? }
+
+  "IsolatedRepo #apply & #packages" should "provide package information using Source" in { ??? }
+
+  "IsolatedRepo#add" should "push object to file system and create a package from it" in { ??? }
+
+  "IsolatedRepo#remove" should "remove object from fs and its package" in { ??? }
+
+  "IsolatedRepo#change" should "rename package if name is changed" in { ??? }
+
+  it should "install/uninstall package if its status changes" in { ??? }
+
+  it should "update status of every package content" in { ??? }
 }
