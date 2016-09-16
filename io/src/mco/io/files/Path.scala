47cef0ce8f33c6040ce7ef04e4cde2f960182a22
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2016-09-16 18:36:45 +0300

    Fixed issues in Path class

--- io/src/mco/io/files/Path.scala
+++ io/src/mco/io/files/Path.scala
@@ -8,8 +8,8 @@ case class Path(str: String) {
   private[files] def f: File = File(str)
   def relativeToS(other: Path): String = other.f.relativize(f).toString
   def asString: String = str
-  def fileName: String = normalized.drop(normalized.indexOf("/") + 1)
-  def /(right: String) = Path(s"$normalized/$right")
+  def fileName: String = normalized.drop(normalized.lastIndexOf("/") + 1)
+  def /(right: String): Path = Path(s"$normalized/$right")
 
   // $COVERAGE-OFF$Equals-hashcode is by contract, even though it's not used in all cases
   override def equals(other: Any): Boolean = other match {
