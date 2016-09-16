faf890bdadb842a040cea09af07c09333fdc2263
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2016-09-16 18:36:34 +0300

    Removed old runner code

--- io/src/mco/shell/CLI.scala
+++
@@ -1,7 +0,0 @@
-package mco.shell
-
-object CLI extends App {
-//  StdIn.readLine("Nothing is implemented yet. Press <Enter> to exit")
-//  println(Harbinger.loadMedia("mco.core.basics.media.ArchiveMedia"))
-//  println(Harbinger.loadPackage("mco.core.basics.BasicPackage"))
-}
--- io/src/mco/shell/Test.scala
+++ io/src/mco/shell/Test.scala
@@ -8,28 +8,6 @@ import mco.io._
 
 //noinspection TypeAnnotation
 object Test extends App {
-//  val src = FolderSource(
-//    File("E:\\Documents\\EA Games\\The Sims 2\\__mco_instance__\\installers"),
-//    ArchiveMedia, FolderMedia
-//  )
-//  val Repo = new Repository(
-//    new FolderTarget(File("E:\\__tmp__\\tempdir")),
-//    IO.unsafePerform(SimplePackages(src))
-//  )
-//  val io = for {
-//    sourceOption <- FolderSource(
-//      File("E:\\Documents\\EA Games\\The Sims 2\\__mco_instance__\\installers"),
-//      ArchiveMedia, FolderMedia
-//    )
-//    packages <- SimplePackages(sourceOption.get)
-//    repo = new Repository(new IsolatedTarget(File("E:\\__tmp__\\tempdir")), packages)
-//    installed <- repo.install("62-Bet on it.zip")
-//  } yield installed
-//
-////  val printio = io.map(r => r.all mkString "\n").map(println)
-//
-//  IO.unsafePerform(io)
-
   val start = System.nanoTime()
 
   val srcIO = FolderSource("E:\\Documents\\EA Games\\The Sims 2\\__mco_instance__\\installers",
@@ -49,5 +27,4 @@ object Test extends App {
 
   println(unsafePerformIO(res))
   println(s"Took ${(System.nanoTime() - start) / 1000 / 1000}ms")
-
 }
