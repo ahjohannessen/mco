6e1b0fe829ffec9c43b2cac4a9ee89b373bd7f20
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2016-09-19 21:22:51 +0300

    Disabled Test.scala. It might be deleted later.

--- io/src/mco/shell/Test.scala
+++ io/src/mco/shell/Test.scala
@@ -1,30 +1,30 @@
-package mco.shell
-
-import mco.io.files.{IO, Path, unsafePerformIO}
-import cats.syntax.foldable._
-import cats.instances.list._
-import mco.Repository
-import mco.io._
-
-//noinspection TypeAnnotation
-object Test extends App {
-  val start = System.nanoTime()
-
-  val srcIO = FolderSource("E:\\Documents\\EA Games\\The Sims 2\\__mco_instance__\\installers",
-    Classifiers.enableAll, ArchiveMedia, FolderMedia).map(_.get)
-
-  val res = for {
-    src <- srcIO
-    repo <- IsolatedRepo(
-      src,
-      Path("E:\\__tmp__\\tempdir"),
-      ()
-    )
-    pkgs = repo.packages.slice(0, 40).map(_.key).toList
-    _ = println(repo.packages.slice(0, 40).map(_.isInstalled))
-    rr <- pkgs.foldM[IO, Repository.Aux[IO, Path, Unit]](repo)((repo, key) => repo.change(key, repo(key).copy(isInstalled = true)))
-  } yield rr.packages.slice(0, 40).map(_.isInstalled).toList
-
-  println(unsafePerformIO(res))
-  println(s"Took ${(System.nanoTime() - start) / 1000 / 1000}ms")
-}
+//package mco.shell
+//
+//import mco.io.files.{IO, unsafePerformIO}
+//import cats.syntax.foldable._
+//import cats.instances.list._
+//import mco.Repository
+//import mco.io._
+//
+////noinspection TypeAnnotation
+//object Test extends App {
+//  val start = System.nanoTime()
+//
+//  val srcIO = FolderSource("E:\\Documents\\EA Games\\The Sims 2\\__mco_instance__\\installers",
+//    Classifiers.enableAll, ArchiveMedia, FolderMedia).map(_.get)
+//
+//  val res = for {
+//    src <- srcIO
+//    repo <- IsolatedRepo(
+//      src,
+//      "E:\\__tmp__\\tempdir",
+//      Vector()
+//    )
+//    pkgs = repo.packages.slice(0, 40).map(_.key).toList
+//    _ = println(repo.packages.slice(0, 40).map(_.isInstalled))
+//    rr <- pkgs.foldM[IO, Repository[IO]](repo)((repo, key) => repo.change(key, repo(key).copy(isInstalled = true)))
+//  } yield rr.packages.slice(0, 40).map(_.isInstalled).toList
+//
+//  println(unsafePerformIO(res))
+//  println(s"Took ${(System.nanoTime() - start) / 1000 / 1000}ms")
+//}
