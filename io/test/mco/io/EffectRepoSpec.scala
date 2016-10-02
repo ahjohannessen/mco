2c0464518e7395482f0df01524be7f346cf0fbcc
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2016-10-02 21:17:35 +0300

    Finished EffectRepoSpec

--- io/test/mco/io/EffectRepoSpec.scala
+++ io/test/mco/io/EffectRepoSpec.scala
@@ -1,15 +1,59 @@
 package mco.io
 
-import mco.UnitSpec
+import cats._
+import cats.data.Xor
+import mco.Fail.Uncaught
+import mco.{Fail, Package, Repository, UnitSpec}
 
 class EffectRepoSpec extends UnitSpec {
-//  "EffectRepo#state" should "just be Unit value" in {
-//
-//  }
-//
-//  def nat = new (State[Int, ?] ~> Task) {
-//    override def apply[A](fa: State[Int, A]): Task[A] = ???
-//  }
-//
-//  def stub: Repository[State[Int, ?], ()] = ???
+  "EffectRepo#state" should "just be Unit value" in {
+    val repo = new EffectRepo(stub, nat)
+    repo.state should be (())
+  }
+
+  "EffectRepo#apply and packages" should "delegate methods to wrapped repo" in {
+    val repo = stub
+    val effect = new EffectRepo(repo, nat)
+    for (s <- 1 to 10) effect(s.toString) should equal (repo(s.toString))
+    effect.packages should equal (repo.packages)
+  }
+
+  "EffectRepo#change, add and remove" should "execute wrapped operations with side-effects" in {
+    val repo = new EffectRepo(stub, nat)
+    repo.change("foo", x => x)
+    sideEffect should be ("change")
+
+    repo.add("bar")
+    sideEffect should be ("add")
+
+    repo.remove("eggs")
+    sideEffect should be ("remove")
+  }
+
+  private var sideEffect: String = ""
+
+  private def stub: Repository[Eval, Int] = new Repository[Eval, Int] {
+    override def state: Int = 42
+    override def apply(key: String): Package = Package(key, Set())
+    override def packages: Traversable[Package] = Seq(Package("1", Set()), Package("2", Set()))
+
+    override def change(oldKey: String, updates: Package): Eval[Self] = Eval.always {
+      sideEffect = "change"
+      stub
+    }
+
+    override def add(f: String): Eval[Fail Xor Self] = Eval.always {
+      sideEffect = "add"
+      Xor.right(stub)
+    }
+
+    override def remove(s: String): Eval[Fail Xor Self] = Eval.always {
+      sideEffect = "remove"
+      Xor.left(Uncaught(new Exception))
+    }
+  }
+
+  private def nat = new (Eval ~> Id) {
+    override def apply[A](fa: Eval[A]): Id[A] = fa.value
+  }
 }
