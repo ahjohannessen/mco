aa76a41f193d6e38e98707d29d09788b9c25aedd
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2016-10-05 00:06:45 +0300

    Removed mutable state from EffectRepoSpec

--- core/test/mco/EffectRepoSpec.scala
+++ core/test/mco/EffectRepoSpec.scala
@@ -1,56 +1,43 @@
 package mco
 
 import cats._
+import cats.data.Const
 
 class EffectRepoSpec extends UnitSpec {
   "EffectRepo#state" should "just be Unit value" in {
-    val repo = new EffectRepo(stub, nat)
+    val repo = new EffectRepo[(String, ?), Const[String, ?], Int](stub, nat)
     repo.state should be (())
   }
 
   "EffectRepo#apply and packages" should "delegate methods to wrapped repo" in {
     val repo = stub
-    val effect = new EffectRepo(repo, nat)
+    val effect = new EffectRepo[(String, ?), Const[String, ?], Int](repo, nat)
     for (s <- 1 to 10) effect(s.toString) should equal (repo(s.toString))
     effect.packages should equal (repo.packages)
   }
 
   "EffectRepo#change, add and remove" should "execute wrapped operations with side-effects" in {
-    val repo = new EffectRepo(stub, nat)
-    repo.change("foo", x => x)
-    sideEffect should be ("change")
+    val repo = new EffectRepo[(String, ?), Const[String, ?], Int](stub, nat)
+    repo change ("foo", x => x) shouldBe Const("change")
 
-    repo.add("bar")
-    sideEffect should be ("add")
+    repo add "bar" shouldBe Const("add")
 
-    repo.remove("eggs")
-    sideEffect should be ("remove")
+    repo remove "eggs" shouldBe Const("remove")
   }
 
-  private var sideEffect: String = ""
-
-  private def stub: Repository[Eval, Int] = new Repository[Eval, Int] {
+  private def stub: Repository[(String, ?), Int] = new Repository[(String, ?), Int] {
     override def state: Int = 42
     override def apply(key: String): Package = Package(key, Set())
     override def packages: Traversable[Package] = Seq(Package("1", Set()), Package("2", Set()))
 
-    override def change(oldKey: String, updates: Package): Eval[Self] = Eval.always {
-      sideEffect = "change"
-      stub
-    }
+    override def change(oldKey: String, updates: Package): (String, Self) = ("change", stub)
 
-    override def add(f: String): Eval[Self] = Eval.always {
-      sideEffect = "add"
-      stub
-    }
+    override def add(f: String): (String, Self) = ("add", stub)
 
-    override def remove(s: String): Eval[Self] = Eval.always {
-      sideEffect = "remove"
-      stub
-    }
+    override def remove(s: String): (String, Self) = ("remove", stub)
   }
 
-  private def nat = new (Eval ~> Id) {
-    override def apply[A](fa: Eval[A]): Id[A] = fa.value
+  private def nat = new ((String, ?) ~> Const[String, ?]) {
+    override def apply[A](fa: (String, A)): Const[String, A] = Const(fa._1)
   }
 }
